9232643537774550c3bd391843eb383a542c0b5b
Jake Armijo <armijojake@yahoo.com>
2022-04-17 07:05:59 -0600

    chore: python dirct

---
+++ data-structures-py/bst.py
@@ -0,0 +1,31 @@
+from calendar import c
+
+
+class BST:
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+
+  def insert(self, value):
+    currentNode = self
+    while True:
+      if value < currentNode.value:
+        if currentNode.left is None:
+          currentNode.left = BST(value)
+          break
+        else:
+          currentNode = currentNode.left
+      else:
+        if currentNode.right is None:
+          currentNode.right = BST(value)
+          break
+        else:
+          currentNode = currentNode.right
+    return self
+  
+  def contains(self, value):
+    pass
+  
+  def remove(self, value, parent_node = None):
+    pass
