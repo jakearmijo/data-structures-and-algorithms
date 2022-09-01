663e0d0dddc73b3f9faabe66ababd086a4cae1b0
Jake Armijo <armijojake@yahoo.com>
2022-09-01 16:35:11 -0600

    chore: updates

--- data-structures-py/Array-Hashmap/TopKFrequentElements.py
+++ data-structures-py/Array-Hashmap/TopKFrequentElements.py
@@ -9,19 +9,19 @@ class Solution(object):
         #init hash map to track values
         count = {}
         # init array to track frequency of each number
-        frequency = [[] for i in range(len(nums) + 1)]
+        frequency = [ [] for i in range(len(nums) + 1) ]
         # loop over nums and map the value to the hashmap
-        for i in nums:
-          count[i] = 1 + count.get(i, 0)
+        for n in nums:
+          count[n] = 1 + count.get(n, 0)
         # loop over the hashmap items and at the value in the array 
         #in example if value 1 occcurs twice. Then inside the array index 2 will have [1]
-        for i, c in count.items():
+        for n, c in count.items():
           frequency[c].append(i)
         # declare a result array must return last value of frequency
         result = []
         # loop over frequency array backwards and append the values until it is k size
         for i in range(len(frequency) - 1, 0, -1):
-          for i in frequency[i]:
-            result.append(i)
+          for n in frequency[i]:
+            result.append(n)
             if len(result) == k:
               return result
