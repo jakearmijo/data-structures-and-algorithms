2d56b0c6b457b9fa6ed3815abe7c63aa6608dc55
Jake Armijo <armijojake@yahoo.com>
2022-10-11 15:36:16 -0600

    chore: starting graphs

--- data-structures-py/graphs/DepthFirstSearch/depth_first_search.py
+++ data-structures-py/graphs/DepthFirstSearch/depth_first_search.py
@@ -7,11 +7,7 @@ class Node:
   
   def add_child(self,name):
     self.children.append(Node(name))
-
-# Write your code here.
-# each node is a vertex - each line or connection is an edge
-# Time = O(v + e)
-# Space = O(v)
+    
   def depth_first_search(self,array):
     array.append(self.name)
     for child in self.children:
